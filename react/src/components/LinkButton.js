781b7acd8e1090ee0dc6ada1abffa44707ae1dda
Ostap Bodnar <ostapbod674@gmail.com>
2021-04-19 10:40:24 +0300

    Prettier code refactoring

--- react/src/components/LinkButton.js
+++ react/src/components/LinkButton.js
@@ -1,37 +1,35 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {withRouter} from 'react-router'
+import React from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router";
 
 const LinkButton = (props) => {
-    const {
-        history,
-        location,
-        match,
-        staticContext,
-        to,
-        onClick,
-        className,
-        value,
-    } = props
-    return (
-        <button
+  const {
+    history,
+    location,
+    match,
+    staticContext,
+    to,
+    onClick,
+    className,
+    value,
+  } = props;
+  return (
+    <button
+      onClick={(event) => {
+        onClick && onClick(event);
 
-            onClick={(event) => {
-
-                onClick && onClick(event)
-
-                history.push(to)
-            }}
-            className={className}
-            value={value}
-            staticContext={staticContext}
-        />
-    )
-}
+        history.push(to);
+      }}
+      className={className}
+      value={value}
+      staticContext={staticContext}
+    />
+  );
+};
 
 LinkButton.propTypes = {
-    to: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired
-}
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
-export default withRouter(LinkButton)
+export default withRouter(LinkButton);
